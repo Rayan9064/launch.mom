eedcf37bb63383d7eddefb2baeb8e8835916f8f7
Rayan9064 <mohammedrayan9064@gmail.com>
2025-01-13 10:26:59 +0530

    Added more page routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Login from './pages/auth/Login'
 import GitHubCallback from './pages/auth/GitHubCallback'
 import CreateSite from './pages/CreateSite'
+import Template from './pages/Template'
 import Product from './pages/Product'
 import Checkout from './pages/Checkout'
 import OrderConfirmation from './pages/OrderConfirmation'
@@ -17,19 +18,19 @@ function App() {
     // <AuthProvider>
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/auth/github" element={<GitHubCallback />} />
-          <Route path="/create" element={<CreateSite />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/confirmation" element={<OrderConfirmation />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* Catch-all route for unmatched paths */}
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
-        </Routes>
+      <main className="flex-grow"><Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/github" element={<GitHubCallback />} />
+        <Route path="/create" element={<CreateSite />} />
+        <Route path="/template" element={<Template />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/confirmation" element={<OrderConfirmation />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Catch-all route for unmatched paths */}
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
       </main>
       <Footer />
     </div>
